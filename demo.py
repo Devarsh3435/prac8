96d0ce0b70a26a4c7c5f2b1de6f6329722606f8f
Devarsh3435 <98757826+Devarsh3435@users.noreply.github.com>
2023-04-20 08:11:42 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,18 @@
-print("Prac9 Works")
-print("Hello")
-print("Jenkins Git Works")
+pipeline{
+  agent any
+  stages{
+    stage('version') {
+      steps {
+        sh 'python3 --version'
+      }
+    }
+    stage('hello') {
+      steps {
+        sh 'python3 hello.py'
+      }
+    }
+  }
+}
+        
+        
+        
